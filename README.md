d8174a326fe310b95da2403dfe977f8cd29b8a2e
Chetan0402 <chetansharma67g@gmail.com>
2025-05-25 08:22:02 +0530

    refractor: cross-platform support

--- README.md
+++ README.md
@@ -3,18 +3,16 @@
 This Python script is designed to notify the user when their battery is various levels. This can help users extend their battery life by reminding them to charge or unplug their device at the optimal times. (Set the percentage on when to notify by editing the `config.json` using `ConfigBuilder.html`)
 
 # Requirements
-- Python (only tested on 3.10.x right now)
-- Only works on windows
+- Python
 ## Modules required
-- win10toast (`pip install win10toast`)
-- psutil (`pip install psutil`)
+- Run `pip -r requirements.txt`
 
 # Usage
 1. Clone the repository <b>OR</b> download the .ZIP file and extract it.
 2. Install the needed modules.
-3. Run the script using the `RUN_IN_BACKGROUND.bat` or `pythonw battery.py`
+3. Run the script using the `RUN_IN_BACKGROUND.bat`(if windows) or `pythonw battery.py`
 
-The script will start running and will continuously monitor your battery percentage. When the battery reaches 100%, a notification will be displayed indicating that the battery is fully charged. When the battery reaches 80% and charging, another notification will be displayed reminding you to unplug your device. Finally, when the battery percentage drops below 30%, the script will display a notification reminding you to charge your device.
+The script will start running and will continuously monitor your battery percentage. When the battery reaches 100%, a notification will be displayed indicating that the battery is fully charged. When the battery reaches 80% and charging, another notification will be displayed reminding you to unplug your device. Finally, when the battery percentage drops below 30%, the script will display a notification reminding you to charge your device. (Default behavior)
 
 #### Change this behaviour by editing the `config.json` using the `ConfigBuilder.html` website.
 
--- battery.py
+++ battery.py
@@ -1,134 +1,131 @@
-from win32event import CreateMutex
-from win32api import GetLastError
-from winerror import ERROR_ALREADY_EXISTS
-from sys import exit
-from os import getpid, listdir, remove
+import psutil
+try:
+    with open("PID", "r+") as pid_file:
+        pid = int(pid_file.read())
+        process = psutil.Process(pid)
+        if process.is_running() and process.name().find("python") != -1:
+            print("BatteryNotifi is already running")
+            exit(0)
+except psutil.NoSuchProcess as e:
+    print("BatteryNotifi is not running, starting a new instance")
+    with open("PID", "w+") as pid_file:
+        pid_file.write(str(psutil.Process().pid))
+        pid_file.flush()
+except Exception as e:
+    exit(0)
+
+from os import listdir, remove
 from CustomLogger import Logger
+import asyncio
+from desktop_notifier import DesktopNotifier, common
+import json
+from time import sleep
 
 logger = Logger()
 logger.createFile()
 logger.info("Running...")
 
-handle = CreateMutex(None, 1, 'BatteryNotifi')
+notifier = DesktopNotifier()
+last_battery_percent = psutil.sensors_battery().percent
 
-if GetLastError() == ERROR_ALREADY_EXISTS:
-    logger.info("Already existed")
+try:
+    with open('config.json') as json_file:
+        config = json.load(json_file)
+        json_file.close()
+except json.JSONDecodeError as e:
+    asyncio.run(notifier.send("BatteryNotification error", "Syntax Error in config file, check config.json", common.Urgency.Critical))
+    exit(1)
+except FileNotFoundError as e:
+    config_default = """{
+        "range":[
+            {"MAX_VAL":30,"MIN_VAL":0,"MSG":"Put laptop in charging","PLUG":"False"}
+            ],
+        "point":[
+            {"VAL":80,"MSG":"Remove from charging","PLUG":"True"}
+            ],
+        "NOTIFY_WHEN_FULL":"True",
+        "DEL_LOG":"True"
+        }"""
+    with open('config.json', "rw+") as json_file:
+        json_file.write(config_default)
+        config = json.load(json_file)
+        json_file.close()
+    asyncio.run(notifier.send("BatteryNotification", "Config file created, edit config.json as per your needs", common.Urgency.Low))
+
+
+def bool_(thing):
+    return str(thing) in ["True","TRUE","1"]
+
+
+if bool_(config["DEL_LOG"]):
     logger.deleteFile()
-    exit(0)
-else:
-    import psutil
-    from time import sleep
-    from win10toast import ToastNotifier
-    import json
-
-    toast = ToastNotifier()
-    last_battery_percent = psutil.sensors_battery().percent
-    pid_file = open("PID", "w+")
-    pid_file.write(str(getpid()))
-    pid_file.flush()
-
-    try:
-        with open('config.json') as json_file:
-            config = json.load(json_file)
-            json_file.close()
-    except json.JSONDecodeError as e:
-        toast.show_toast("Syntax Error in config file", "Check config.json")
-    except json.JSONDecoder as e:
-        toast.show_toast("Syntax Error in config file", "Check config.json")
-    except FileNotFoundError as e:
-        config_default = """{
-            "range":[
-                {"MAX_VAL":30,"MIN_VAL":0,"MSG":"Put laptop in charging","PLUG":"False"}
-                ],
-            "point":[
-                {"VAL":80,"MSG":"Remove from charging","PLUG":"True"}
-                ],
-            "NOTIFY_WHEN_FULL":"True",
-            "DEL_LOG":"True"
-            }"""
-        with open('config.json', "rw+") as json_file:
-            json_file.write(config_default)
-            config = json.load(json_file)
-            json_file.close()
-        toast.show_toast("Config file created", "Edit config.json as per your needs")
+    for filename in listdir():
+        if filename.startswith("batteryNotifi-") and filename.endswith(".log"):
+            try:
+                remove(filename)
+            finally:
+                pass
 
 
-    def bool_(thing):
-        if str(thing) == "True" or str(thing) == "TRUE" or str(thing) == "1":
-            return True
-        else:
-            return False
+def send_battery_debug(battery):
+    if not bool_(config["DEL_LOG"]):
+        logger.info(f"Battery {battery.percent}")
 
 
-    if bool_(config["DEL_LOG"]):
-        logger.deleteFile()
-        for filename in listdir():
-            if filename.startswith("batteryNotifi-") and filename.endswith(".log"):
-                try:
-                    remove(filename)
-                finally:
-                    pass
+def init_point():
+    config["DONE_FULL_ONCE"] = False
+    for points in config["point"]:
+        points["DONE_ONCE"] = False
 
 
-    def send_battery_debug(battery):
-        if not bool_(config["DEL_LOG"]):
-            logger.info(f"Battery {battery.percent}")
+last_battery_percent = 0
 
 
-    def init_point():
+def main_loop():
+    sleep(1)
+    battery = psutil.sensors_battery()
+    global last_battery_percent
+    global config
+
+    if bool_(config["NOTIFY_WHEN_FULL"]) and battery.percent == 100 and not bool_(
+            config["DONE_FULL_ONCE"]) and battery.power_plugged:
+        asyncio.run(notifier.send("BatteryNotification", "Laptop battery at 100%, please remove from charging"))
+        config["DONE_FULL_ONCE"] = True
+
+    for points in config["point"]:
+        if int(points["VAL"]) == battery.percent and not points["DONE_ONCE"] and battery.power_plugged == bool_(
+                points["PLUG"]):
+            asyncio.run(notifier.send(f"Laptop battery at {battery.percent}", points["MSG"]))
+            points["DONE_ONCE"] = True
+            last_battery_percent = battery.percent
+            send_battery_debug(battery)
+
+    for ranges in config["range"]:
+        if int(ranges["MIN_VAL"]) <= battery.percent <= int(ranges[
+            "MAX_VAL"]) and last_battery_percent != battery.percent and battery.power_plugged == bool_(
+            ranges["PLUG"]):
+            asyncio.run(notifier.send(f"Laptop battery at {battery.percent}", ranges["MSG"]))
+            send_battery_debug(battery)
+
+    if last_battery_percent != battery.percent:
         config["DONE_FULL_ONCE"] = False
         for points in config["point"]:
             points["DONE_ONCE"] = False
+        last_battery_percent = battery.percent
+
+    try:
+        with open("battery.reload") as reload:
+            with open('config.json') as json_file:
+                config = json.load(json_file)
+                json_file.close()
+                init_point()
+            reload.close()
+        remove("battery.reload")
+        send_battery_debug("Config reloaded")
+    except FileNotFoundError:
+        pass
 
 
-    last_battery_percent = 0
-
-
-    def main_loop():
-        sleep(1)
-        battery = psutil.sensors_battery()
-        global last_battery_percent
-        global config
-
-        if bool_(config["NOTIFY_WHEN_FULL"]) and battery.percent == 100 and not bool_(
-                config["DONE_FULL_ONCE"]) and battery.power_plugged:
-            toast.show_toast("Laptop abttery at 100%", "Please remove from charging")
-            config["DONE_FULL_ONCE"] = True
-
-        for points in config["point"]:
-            if int(points["VAL"]) == battery.percent and not points["DONE_ONCE"] and battery.power_plugged == bool_(
-                    points["PLUG"]):
-                toast.show_toast(f"Laptop battery at {battery.percent}", points["MSG"])
-                points["DONE_ONCE"] = True
-                last_battery_percent = battery.percent
-                send_battery_debug(battery)
-
-        for ranges in config["range"]:
-            if int(ranges["MIN_VAL"]) <= battery.percent <= int(ranges[
-                "MAX_VAL"]) and last_battery_percent != battery.percent and battery.power_plugged == bool_(
-                ranges["PLUG"]):
-                toast.show_toast(f"Laptop battery at {battery.percent}", ranges["MSG"])
-                send_battery_debug(battery)
-
-        if last_battery_percent != battery.percent:
-            config["DONE_FULL_ONCE"] = False
-            for points in config["point"]:
-                points["DONE_ONCE"] = False
-            last_battery_percent = battery.percent
-
-        try:
-            with open("battery.reload") as reload:
-                with open('config.json') as json_file:
-                    config = json.load(json_file)
-                    json_file.close()
-                    init_point()
-                reload.close()
-            remove("battery.reload")
-            send_battery_debug("Config reloaded")
-        except FileNotFoundError:
-            pass
-
-
-    init_point()
-    while True:
-        main_loop()
+init_point()
+while True:
+    main_loop()
